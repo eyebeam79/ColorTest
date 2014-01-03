921f45e05ac2e96c4d2104c03565d118c327bb87
SDT1 <eyebeam79@naver.com>
2014-01-03 16:37:58 +0900

    예제 완료했습니다.

--- ColorTest/ViewController.m
+++ ColorTest/ViewController.m
@@ -9,15 +9,54 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UIView *colorView;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
 @implementation ViewController
+- (IBAction)onColorSliderChanged:(id)sender
+{
+    
+    if ([self.activityIndicator isAnimating])
+    {
+        [self.activityIndicator stopAnimating];
+        NSLog(@"indicator changed");
+    }
+    
+    self.colorView.backgroundColor = [UIColor colorWithRed:self.redSlider.value green:self.greenSlider.value blue:self.blueSlider.value alpha:1.0];
+}
+- (IBAction)powerOnOff:(id)sender
+{
+    BOOL isOn = ((UISwitch *)sender).on;
+    
+    self.redSlider.enabled = isOn;
+    self.greenSlider.enabled = isOn;
+    self.blueSlider.enabled = isOn;
+    
+    if (isOn)
+    {
+        [self.activityIndicator stopAnimating];
+        
+        [self performSelector:@selector(onColorSliderChanged:) withObject:nil afterDelay:1.0];
+        
+    }
+    else
+    {
+        self.colorView.backgroundColor = [UIColor grayColor];
+    }
+
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self onColorSliderChanged:nil];
 }
 
 - (void)didReceiveMemoryWarning
